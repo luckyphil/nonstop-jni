f21c5b84dc6994f82d015b020c6d10ffdb635428
Phil <phil.herscovics@gmail.com>
2012-03-15 21:18:30 +1100

    Initial

---
+++ java/src/test/java/org/itug/nonstopjni/GuardianProcedureTest.java
@@ -0,0 +1,21 @@
+package org.itug.nonstopjni;
+import org.junit.*;
+import static org.junit.Assert.*;
+
+
+public class GuardianProcedureTest {
+
+
+
+
+	@Test
+	public void testget48BitTimestamp() {
+		GuardianProcedure gp = new GuardianProcedure();
+		
+		short timestamp[] = null;
+		timestamp = gp.get48BitTimestamp();
+		assertNotNull("get48BitTimestamp", timestamp);
+		assertEquals("timestamp length", 3, timestamp.length );
+        System.out.println("Timestamp Words "  + timestamp[0] + ":" + timestamp[1]+":" + timestamp[2]);
+	}
+}
